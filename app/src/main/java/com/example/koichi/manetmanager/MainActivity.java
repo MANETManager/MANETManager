9ccd5d4c4b478ecfbde60928fd2e6929540b842f
setohima <raito.1261@gmail.com>
2017-08-09 12:00:11 +0900

    add Main Act: Facebook認証を実装。それに成功するとメニュー画面に遷移できる。

--- app/src/main/java/com/example/koichi/manetmanager/MainActivity.java
+++ app/src/main/java/com/example/koichi/manetmanager/MainActivity.java
@@ -1,19 +1,50 @@
 package com.example.koichi.manetmanager;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+//Facebook
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.CallbackManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+//Toast notify
+import android.widget.Toast;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class MainActivity extends AppCompatActivity {
+
+    LoginButton loginButton;
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
         setContentView(R.layout.activity_main);
 
-        //FB認証(今は簡易的にボタンで画面遷移)
+        //新FB認証
+        /*ログインボタン設置、要求するパーミッションの設定*/
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("user_managed_groups");
+        callbackManager = CallbackManager.Factory.create();
+
+        //旧FB認証(今は簡易的にボタンで画面遷移)
         Button authentication_Button = (Button) findViewById(R.id.authentication);
         authentication_Button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +56,33 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Toast toast = Toast.makeText(MainActivity.this, "ログイン成功", Toast.LENGTH_SHORT);
+                toast.show();
+                Intent intent = new Intent(getApplication(), MenuActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onCancel() {
+                Toast toast = Toast.makeText(MainActivity.this, "ログインを中止しました", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                Toast toast = Toast.makeText(MainActivity.this, "ログインに失敗しました", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+    }
 
 }
--- app/src/main/java/com/example/koichi/manetmanager/MenuActivity.java
+++ app/src/main/java/com/example/koichi/manetmanager/MenuActivity.java
@@ -6,14 +6,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import android.util.Log;
+
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+
+
 public class MenuActivity extends AppCompatActivity {
+
+    private static final String TAG = "MenuActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        //グループ一覧
+                //グループ一覧
         Button group_Button = (Button) findViewById(R.id.group);
         group_Button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,5 +44,41 @@ public class MenuActivity extends AppCompatActivity {
 
             }
         });
+
+        //GraphAPIテスト
+        Button btnGraphApiTest = (Button) findViewById(R.id.btnGraphApiTest);
+        btnGraphApiTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.btnGraphApiTest:
+                        GraphApiTest();
+                        break;
+
+                }
+
+            }
+        });
+
     }
+
+    private void GraphApiTest() {
+        /*
+        Bundle params = new Bundle();
+        params.putString("message", "This is a test message");
+        */
+        //GET
+        new GraphRequest(
+                AccessToken.getCurrentAccessToken(),
+                "/1752679538356004/feed",
+                null,
+                HttpMethod.GET,
+                new GraphRequest.Callback() {
+                    public void onCompleted(GraphResponse response) {
+                        Log.i(TAG, response.getJSONObject().toString());
+                    }
+                }
+        ).executeAsync();
+    }
+
 }
